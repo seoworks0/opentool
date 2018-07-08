7d3a92fc7eaa686b45c94bd110b8501cdb2bf949
seoworks0 <37975126+seoworks0@users.noreply.github.com>
2018-07-08 19:35:17 +0900

    Update settings.py

--- opentool/settings.py
+++ opentool/settings.py
@@ -42,7 +42,7 @@ SECRET_KEY = 'b-&c(+uy1sb(2(dq_*j3lesou&3c&w6ileqoc+xf7f6w3wzusu'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://silly-brahmagupta-1755.arukascloud.io']
+ALLOWED_HOSTS = ['silly-brahmagupta-1755.arukascloud.io']
 
 
 # Application definition
